29cc2726bf084ef7615cf9190ad7d241d9a81228
Michael Bohn <michael.bohn@freiheit.com>
2018-01-04 17:46:12 +0100

    update build script to latest rust docker version

--- pi_scripts/build.sh
+++ pi_scripts/build.sh
@@ -1,1 +1,1 @@
-docker run  --volume ~/pprojects/rust-HD44780:/home/cross/project ragnaroek/rust-raspberry:1.13.0 build
+docker run  --volume ~/pprojects/rust-HD44780:/home/cross/project ragnaroek/rust-raspberry:1.23.0 build
